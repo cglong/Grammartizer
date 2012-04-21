c2c6a3973f7b14e3a6ba0f526d95d12a73e665cb
Chris Long <chrislong@gatech.edu>
2012-04-21 19:31:15 -0400

    Rules can create new Expressions

--- src/com/github/cglong/Grammartizer/Expression.java
+++ src/com/github/cglong/Grammartizer/Expression.java
@@ -1,15 +1,14 @@
 package com.github.cglong.Grammartizer;
 
 import java.util.List;
-import java.util.ArrayList;
 
 public class Expression {
 	private List<Symbol> rightSymbols;
 	private Nonterminal leftSymbol;
 	
-	public Expression(Nonterminal leftSymbol) {
-		this.rightSymbols = new ArrayList<Symbol>();
+	public Expression(Nonterminal leftSymbol, List<Symbol> rightSymbols) {
 		this.leftSymbol = leftSymbol;
+		this.rightSymbols = rightSymbols;
 	}
 	
 	public boolean hasLeftRecursion() {
--- src/com/github/cglong/Grammartizer/Rule.java
+++ src/com/github/cglong/Grammartizer/Rule.java
@@ -1,6 +1,7 @@
 package com.github.cglong.Grammartizer;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.ArrayList;
 
 public class Rule {
@@ -10,5 +11,10 @@ public class Rule {
 	public Rule(Nonterminal symbol) {
 		this.expressions = new ArrayList<Expression>();
 		this.symbol = symbol;
+	}
+	
+	public void addExpression(List<Symbol> expression) {
+		Expression newExpression = new Expression(this.symbol, expression);
+		this.expressions.add(newExpression);
 	}
 }
