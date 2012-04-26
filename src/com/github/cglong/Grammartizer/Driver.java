7d9e63081199cd2e4b23d6464d272296a08eb742
Vlad <vlasmirnov@gmail.com>
2012-04-25 22:53:45 -0400

    for I shall commit X

---
+++ src/com/github/cglong/Grammartizer/Driver.java
@@ -0,0 +1,51 @@
+package com.github.cglong.Grammartizer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Driver {
+
+	public static void validate(ArrayList<Terminal> inputtokens, Nonterminal startvar, ParseTable parsetable)
+	{
+		
+		Stack<Symbol> stack = new Stack<Symbol>();
+		stack.push(startvar);
+		
+		while(!stack.empty())
+		{
+			Symbol sym = stack.pop();
+			if(!sym.getName().equals(""))
+			{
+				if(sym.isTerminal())
+				{
+					if(sym.equals(inputtokens.get(0)))
+						inputtokens.remove(0);
+					else
+						fail("Unexpected Token: " + inputtokens.get(0).getName());			
+				}
+				else
+				{
+					Rule r = parsetable.get((Nonterminal)sym, inputtokens.get(0));
+					if(r==null)
+						fail("Unexpected Token: " + inputtokens.get(0).getName());
+					
+					List<Symbol> rightsymbols = r.getRightSymbols();
+					for(int a = rightsymbols.size()-1; a > -1; a--)
+						stack.push(rightsymbols.get(a));
+				}			
+			}
+		}//end while
+		
+		if(!inputtokens.isEmpty())
+			fail("Unexpected Token: " + inputtokens.get(0).getName());
+		System.out.println("Successful parse");
+		
+	}//end validate
+	
+	private static void fail(String s)
+	{
+		System.out.println(s);
+		System.exit(-1);
+	}
+}//end class
